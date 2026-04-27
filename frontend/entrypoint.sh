3ea71df37406f50f6fabcb7b42c131ca013386a0
Lukáš Kasič <src@lksc.sk>
2026-04-27 13:36:10 +0200

    fix: frontend nginx lazy resolve

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
 
-envsubst '${BACKEND_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+RESOLVER=$(awk 'NR==1{print $2}' /etc/resolv.conf)
+export RESOLVER
+
+envsubst '${BACKEND_URL} ${RESOLVER}' \
+  < /etc/nginx/nginx.conf.template \
+  > /etc/nginx/conf.d/default.conf
+
 exec nginx -g 'daemon off;'
