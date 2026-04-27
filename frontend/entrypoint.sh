42abf33a801bb8cc4ce875ec455aaa1fc918af8b
Lukáš Kasič <src@lksc.sk>
2026-04-27 14:29:27 +0200

    Revert "fix: frontend nginx lazy resolve"
    This reverts commit 3ea71df37406f50f6fabcb7b42c131ca013386a0.

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -e
 
-RESOLVER=$(awk 'NR==1{print $2}' /etc/resolv.conf)
-export RESOLVER
-
-envsubst '${BACKEND_URL} ${RESOLVER}' \
-  < /etc/nginx/nginx.conf.template \
-  > /etc/nginx/conf.d/default.conf
-
+envsubst '${BACKEND_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
